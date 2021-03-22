31222465a7f2b24a08f6fe89a2b312586c0ddbaf
hechenglong <kfhechenglong@126.com>
2021-03-22 15:48:33 +0800

    初始化

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,52 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div>{{test}}</div>
+    <button @click="buttonClickHanlder">点击按钮</button>
+    {{computedVal}}
+    <div>
+      {{testData}}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref, Ref, watch, watchEffect, computed, ComputedRef } from 'vue'
 
 export default defineComponent({
   name: 'HelloWorld',
+  data () {
+    return {
+      testData: 'testData'
+    }
+  },
   props: {
-    msg: String
+    msg: null
+  },
+  setup (prop) {
+    console.log(prop.msg)
+    // const { msg }= prop
+    console.log('setup')
+    const val: Ref<number | string> = ref(0)
+    const buttonClickHanlder = () => {
+      val.value = (val.value as number) + 1
+    }
+    watchEffect(() => {
+      console.log(val.value)
+    })
+    watch(val, (a, b) => {
+      console.log(a, b)
+    })
+    const computedVal: ComputedRef<number | string> = computed(() => {
+      if (typeof val.value === 'number') {
+        return val.value + 1
+      }
+      return val.value + 'tets'
+    })
+    return {
+      test: val,
+      computedVal: computedVal,
+      buttonClickHanlder: buttonClickHanlder
+    }
   }
 })
 </script>
